4bd4f442e464a6ac9b0e8751bec22b03102e2daa
Isaac Dremora <biohazard566@gmail.com>
2023-09-22 21:05:37 +0500

    Done

--- context/contextDb.cs
+++ context/contextDb.cs
@@ -5,6 +5,9 @@ namespace ContextDb;
 
 class FoodPositionDb : DbContext // класс контекста. Нужен, как инструкция для EF. Шото типа сеанса, за который сохраняются аттрибуты из класса выше.
 {
-    public FoodPositionDb(DbContextOptions options) : base(options) {}
+    public FoodPositionDb(DbContextOptions options) : base(options) 
+    {
+        
+    }
     public DbSet<FoodPosition> positions {get; set;} = null!;
 }
